321b6c1b191f334df6505723315e2c6ccf1de787
Dmytro Khomenko <82578557+choodoss@users.noreply.github.com>
2023-10-09 21:56:09 +0300

    revised the menu a little

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-// import richList from './index.json';
+async function getRichList() {
+    try {
+        const response = await fetch('./index.json');
+        const richList = await response.json();
+        return richList;
+    } catch (error) {
+        console.error('Error fetching rich list:', error);
+        throw error;
+    }
+}
 
-// const mainList = document.querySelector('.js-rich-list');
+const makeMarkup = (richList) => {
+    return richList.map(({ name, link, photo }) => (
+        `<li class="rich-item">
+            <a class="rich-link" href=${link}>
+                <img class="rich-link__img" src="${photo}" alt=${name}>
+                <h4 class="rich-link__title">${name}</h4>
+            </a>
+        </li>`
+    )).join('');
+}
 
-// console.log(mainList);
-// console.log(richList);
+const insertRichList = async () => {
+    try {
+        const { richList } = await getRichList();
+        const mainList = document.querySelector('.js-rich-list');
+        mainList.innerHTML = makeMarkup(richList);
+    } catch (error) {
+        console.error('Error inserting rich list:', error);
+    }
+}
+
+insertRichList();
+
+
+
